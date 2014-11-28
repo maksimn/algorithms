8b261b27defba4604e943f7cddb3b0ac860843bb
maksimn <maksim1989sovunion@yandex.ru>
2014-11-28 21:51:10 +0300

    Generalize arrays

--- Sorting/SortingExamples/Program.cs
+++ Sorting/SortingExamples/Program.cs
@@ -2,53 +2,33 @@
 using System.Collections.Generic;
 
 static class ArraySortingExtensions {
-    public static void BubbleSort(this int[] a) {
+    public static void BubbleSort<T>(this T[] a, IComparer<T> cmp) {
         int n = a.Length;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n - 1; j++) {
-                if (a[j] > a[j + 1]) {
+                if (cmp.Compare(a[j], a[j + 1]) < 0) {
                     Swap(ref a[j], ref a[j + 1]);
                 }
             }
         }
     }
 
-    public static void Swap(ref int a, ref int b) {
-        int t = a;
+    public static void Swap<T>(ref T a, ref T b) {
+        T t = a;
         a = b;
         b = t;
     }
 
-    public static void ConsolePrint(this int[] a) {
-        foreach (int x in a) {
+    public static void ConsolePrint<T>(this T[] a) {
+        foreach (T x in a) {
             Console.Write(x + " ");
-        }
-        Console.WriteLine();
-    }
-        
-    public static void BubbleSort<T>(this List<T> a, Comparer<T> cmp) {
-        int n = a.Count;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n - 1; j++) {
-                if (cmp.Compare(a[j], a[j + 1]) < 0) {
-                    T t = a[j];
-                    a[j] = a[j + 1];
-                    a[j + 1] = t;
-                }
-            }
-        }
-    }
-
-    public static void ConsolePrint<T>(this List<T> a) {
-        for (int i = 0; i < a.Count; i++) {
-            Console.Write(a[i] + " ");
         }
         Console.WriteLine();
     }
 }
 
-public class IntComparer : Comparer<int> {
-    public override int Compare(int x, int y) {
+public class IntAscComparer : IComparer<int> {
+    public int Compare(int x, int y) {
         return y - x;
     }
 }
@@ -56,15 +36,7 @@ public class IntComparer : Comparer<int> {
 class Program {
     static void Main(string[] args) {
         int[] arr = new int[] { 1, 5, 0, 89, 3, 7, 9, 53 };
-        arr.BubbleSort();
+        arr.BubbleSort(new IntAscComparer());
         arr.ConsolePrint();
-
-        List<int> a = new List<int>();
-        Random rand = new Random();
-        for (int i = 0; i < 10; i++) {
-            a.Add(rand.Next(0, 100));
-        }
-        a.BubbleSort(new IntComparer());
-        a.ConsolePrint();
     }
 }
