4a9b25bd8de1d5f0e5f30e8742718c60a697a1e9
maksimn <maksim1989sovunion@yandex.ru>
2015-01-20 09:59:46 +0300

    Suffix Automaton

---
+++ SuffixAutomaton/SuffixAutomaton/Program.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+
+public class SuffixAutomaton {
+    private class State {
+        public Int32 len, link;
+        public SortedDictionary<Char, Int32> next = new SortedDictionary<Char, Int32>();
+    }
+    public Int32 N { get; set; } // число подстрок
+    private List<State> st = new List<State>();
+    public Int32 Size { get; set; }
+    public Int32 Last { get; set; }
+    public SuffixAutomaton() { 
+        st.Add(new State());
+        Size = 1; 
+        Last = 0;
+        st[0].len = 0;
+        st[0].link = -1;
+    }
+    public void Extend(Char c) {
+        Int32 cur = Size++;
+        st.Add(new State());
+        st[cur].len = st[Last].len + 1;
+        Int32 p;
+        for (p = Last; p != -1 && !st[p].next.ContainsKey(c); p = st[p].link) {
+            st[p].next[c] = cur;
+        }
+        if (p == -1) {
+            st[cur].link = 0;
+        } 
+        else {
+            Int32 q = st[p].next[c];
+            if (st[p].len + 1 == st[q].len) {
+                st[cur].link = q;
+            } 
+            else {
+                Int32 clone = Size++;
+                st.Add(new State());
+                st[clone].len = st[p].len + 1;
+                st[clone].next = new SortedDictionary<char,int>(st[q].next);
+                st[clone].link = st[q].link;
+                for (; p != -1 && st[p].next.ContainsKey(c) && st[p].next[c] == q; p = st[p].link) {
+                    st[p].next[c] = clone;
+                }
+                st[q].link = st[cur].link = clone;
+            }
+        }
+        Last = cur;
+    }
+    public void Print() {
+        for (Int32 i = 0; i < Size; i++) {
+            Console.WriteLine("\nСостояние : {0}\nlen = {1}\nlink = {2}\nNext:", i, st[i].len, st[i].link);
+            foreach (var item in st[i].next) {
+                Console.WriteLine("{0} --> {1}", item.Key, item.Value);
+            }
+        }
+    }
+    private Boolean IsAchievableFlag = false;
+    private void OnAchievabilityFound() {
+        IsAchievableFlag = true;
+    }
+    public void IsAchievableAux(Int32 node, Char d, Char[] other) {
+        if (st[node].next.ContainsKey(d)) {
+            OnAchievabilityFound();
+            return; 
+        } else {
+            foreach (var transition in st[node].next) {
+                Boolean isMarkEqualToOther = false;
+                foreach (var otherDelimiter in other) {
+                    if (transition.Key == otherDelimiter) {
+                        isMarkEqualToOther = true;
+                        break;
+                    }
+                }
+                if (isMarkEqualToOther == false) {
+                    IsAchievableAux(transition.Value, d, other);
+                }
+            }
+            return;
+        }
+    }
+    public Boolean IsAchievable(Int32 node, Char d, Char[] other) {
+        if (st[node].next.ContainsKey(d)) {
+            return true;
+        } else {
+            foreach (var transition in st[node].next) {
+                Boolean isMarkEqualToOther = false;
+                foreach (var otherDelimiter in other) {
+                    if (transition.Key == otherDelimiter) {
+                        isMarkEqualToOther = true;
+                        break;
+                    }
+                }
+                if (isMarkEqualToOther == false) {
+                    return IsAchievable(transition.Value, d, other);
+                }
+            }
+            return false;
+        }
+    }
+    public Boolean IsAllAchievable(Int32 node, Char[] delimiters) {
+        for (Int32 i = 0; i < N; i++) {
+            Char ch = delimiters[i];
+            Char[] other = new Char[N - 1];
+            for (Int32 k = 0, j = 0; k < N; k++) {
+                if (delimiters[k] != ch) {
+                    other[j++] = delimiters[k]; 
+                }
+            }
+            if (!IsAchievable(node, ch, other)) {
+                return false;
+            }
+        }
+        return true;
+    }
+    public Int32 FindStateThatMatchesMaxSubstring(Char[] delimiters) {
+        Int32 currMaxState = 0, longest = 0;
+        for (Int32 i = 1; i < Size; i++) {
+            if (IsAllAchievable(i, delimiters) && st[i].len > longest) {
+                currMaxState = i;
+                longest = st[i].len;
+            }
+        }
+        return currMaxState;
+    }
+    public StringBuilder answer = new StringBuilder();
+    public void LongestSubstringBuilder(Int32 node) {
+        for (Int32 i = node - 1; i >= 0; i--) {
+            if(st[i].len + 1 == st[node].len && st[i].next.ContainsValue(node)) {
+                foreach (var trans in st[i].next) {
+                    if (trans.Value == node) {
+                        answer.Append(trans.Key);
+                        break;
+                    }
+                }
+                LongestSubstringBuilder(i);
+            }
+        }
+    }
+    public String GetAnswer() {
+        StringBuilder s = new StringBuilder();
+        for (Int32 i = answer.Length - 1; i >= 0; i--) {
+            s.Append(answer[i]);
+        }
+        return s.ToString();
+    }
+    public String MaxCommonSubstring(Char[] delim) {
+        Int32 state = FindStateThatMatchesMaxSubstring(delim);
+        LongestSubstringBuilder(state);
+        return GetAnswer();
+    }
+}
+
+class Program {
+    static void Main(String[] args) {
+/*
+        SuffixAutomaton sa = new SuffixAutomaton();
+        String line = Console.ReadLine();
+        Int32 n = Convert.ToInt32(line);
+        if (n > 10) {
+            throw new Exception("Error: Wrong number of input strings");
+        }
+        sa.N = n;
+        Char[] delimiter = new Char[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+        Char[] delim = new Char[n];
+        for (Int32 i = 0; i < n; i++) {
+            delim[i] = delimiter[i];
+        }
+            for (Int32 i = 0; i < n; i++) {
+                line = Console.ReadLine();
+                for (Int32 j = 0, length = line.Length; j < length; j++) {
+                    sa.Extend(line[j]);
+                }
+                sa.Extend(delimiter[i]);
+            }
+        //sa.Print();
+        Console.WriteLine(sa.MaxCommonSubstring(delim));
+ */
+        SuffixAutomaton sa = new SuffixAutomaton();
+        String s = "arrc0zrrv1trrn2";
+        sa.N = 3;
+        for (int i = 0; i < s.Length; i++) {
+            sa.Extend(s[i]);
+        }
+        sa.Print();
+    }
+}
---
+++ SuffixAutomaton/UnitTest/UnitTest1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest {
+
+    [TestClass]
+    public class UnitTest1 {
+        [TestMethod]
+        public void IsAchievableTest1() {
+            SuffixAutomaton sa = new SuffixAutomaton();
+            String s = "abc0bca1bbc2";
+            for (int i = 0; i < s.Length; i++) {
+                sa.Extend(s[i]);
+            }
+            Assert.AreEqual(true, sa.IsAchievable(2, '0', new Char[] { '1', '2' }));
+            Assert.AreEqual(false, sa.IsAchievable(2, '1', new Char[] { '0', '2' }));
+            Assert.AreEqual(false, sa.IsAchievable(2, '2', new Char[] { '0', '1' }));
+        }
+        [TestMethod]
+        public void IsAllAchievableTest1() {
+            SuffixAutomaton sa = new SuffixAutomaton();
+            String s = "abc0bca1bbc2"; 
+            for (int i = 0; i < s.Length; i++) {
+                sa.Extend(s[i]);
+            }
+            sa.N = 3;
+            Assert.AreEqual(true, sa.IsAllAchievable(0, new Char[] { '0', '1', '2' }));
+            Assert.AreEqual(false, sa.IsAllAchievable(1, new Char[] { '0', '1', '2' }));
+            Assert.AreEqual(false, sa.IsAllAchievable(2, new Char[] { '0', '1', '2' }));
+        }
+        [TestMethod]
+        public void MaxStateTest() {
+            SuffixAutomaton sa = new SuffixAutomaton();
+            String s = "abc0bca1bbc2";
+            sa.N = 3;
+            for (int i = 0; i < s.Length; i++) {
+                sa.Extend(s[i]);
+            }
+            Assert.AreEqual(8, sa.FindStateThatMatchesMaxSubstring(new char[] { '0', '1', '2' }));
+        }
+        [TestMethod]
+        public void MaxCommonSubstrTest1() {
+            SuffixAutomaton sa = new SuffixAutomaton();
+            String s = "aaa0aaa1aaa2";
+            sa.N = 3;
+            for (int i = 0; i < s.Length; i++) {
+                sa.Extend(s[i]);
+            }
+            Assert.AreEqual("aaa", sa.MaxCommonSubstring(new Char[] { '0', '1', '2' }));
+        }
+        [TestMethod]
+        public void MaxCommonSubstrTest2() {
+            SuffixAutomaton sa = new SuffixAutomaton();
+            String s = "arrc0zrrv1trrn2";
+            sa.N = 3;
+            for (int i = 0; i < s.Length; i++) {
+                sa.Extend(s[i]);
+            }
+            Assert.AreEqual("rr", sa.MaxCommonSubstring(new Char[] { '0', '1', '2' }));
+        }
+        [TestMethod]
+        public void MaxStateTest2() {
+            SuffixAutomaton sa = new SuffixAutomaton();
+            String s = "arrc0zrrv1trrn2";
+            sa.N = 3;
+            for (int i = 0; i < s.Length; i++) {
+                sa.Extend(s[i]);
+            }
+            Assert.AreEqual(10, sa.FindStateThatMatchesMaxSubstring(new char[] { '0', '1', '2' }));
+        }
+        [TestMethod]
+        public void IsAllAchievableTest2() {
+            SuffixAutomaton sa = new SuffixAutomaton();
+            String s = "arrc0zrrv1trrn2";
+            sa.N = 3;
+            for (int i = 0; i < s.Length; i++) {
+                sa.Extend(s[i]);
+            }
+            Assert.AreEqual(true, sa.IsAllAchievable(0, new Char[] { '0', '1', '2' }));
+            Assert.AreEqual(true, sa.IsAllAchievable(10, new Char[] { '0', '1', '2' }));
+            Assert.AreEqual(false, sa.IsAllAchievable(12, new Char[] { '0', '1', '2' }));
+        }
+        [TestMethod]
+        public void IsAchievableTest2() {
+            SuffixAutomaton sa = new SuffixAutomaton();
+            String s = "arrc0zrrv1trrn2";
+            sa.N = 3;
+            for (int i = 0; i < s.Length; i++) {
+                sa.Extend(s[i]);
+            }
+            Assert.AreEqual(true, sa.IsAchievable(10, '0', new Char[] { '1', '2' }));
+            Assert.AreEqual(true, sa.IsAchievable(10, '1', new Char[] { '0', '2' }));
+//            Assert.AreEqual(true, sa.IsAchievable(10, '2', new Char[] { '0', '1' }));
+        }
+    }
+}
