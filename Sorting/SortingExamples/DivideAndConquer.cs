8fdc98eb52fd31d05a6e30ddc9d3d217fc852410
maksimn <maksim1989sovunion@yandex.ru>
2015-03-12 08:50:36 +0300

    Working with Submatrix Arythmetics

--- Sorting/SortingExamples/DivideAndConquer.cs
+++ Sorting/SortingExamples/DivideAndConquer.cs
@@ -123,14 +123,85 @@ public class Matrix<T> {
     }
     private static void StrassenMultiplication(Matrix<T> a, Matrix<T> b, Matrix<T> c) {
         Int32 n = a.NumRows;
-        var aInd = new SubmatrixIndices() { i_max = n, i_min = 1, j_max = n, j_min = 1 };
+        var aInd = new SubmatrixIndices() { i_min = 1, j_min = 1, n = n };
         StrassenMultiplication(a, aInd, b, aInd, c, aInd);
     }
     private static void StrassenMultiplication(Matrix<T> a, SubmatrixIndices aIndices, 
         Matrix<T> b, SubmatrixIndices bIndices, Matrix<T> c, SubmatrixIndices cIndices) {
-
+        // 1. Разбиение на подматрицы
+        Int32 sz = aIndices.n / 2;
+        SubmatrixIndices a11 = new SubmatrixIndices() { 
+            i_min = aIndices.i_min, j_min = aIndices.j_min, n = sz 
+        };
+        SubmatrixIndices a12 = new SubmatrixIndices() {
+            i_min = aIndices.i_min, j_min = aIndices.j_min + sz, n = sz
+        };
+        SubmatrixIndices a21 = new SubmatrixIndices() {
+            i_min = aIndices.i_min + sz, j_min = aIndices.j_min, n = sz
+        };
+        SubmatrixIndices a22 = new SubmatrixIndices() {
+            i_min = aIndices.i_min + sz, j_min = aIndices.j_min + sz, n = sz
+        };
+        SubmatrixIndices b11 = new SubmatrixIndices() {
+            i_min = bIndices.i_min, j_min = bIndices.j_min, n = sz
+        };
+        SubmatrixIndices b12 = new SubmatrixIndices() {
+            i_min = bIndices.i_min, j_min = bIndices.j_min + sz, n = sz
+        };
+        SubmatrixIndices b21 = new SubmatrixIndices() {
+            i_min = bIndices.i_min + sz, j_min = bIndices.j_min, n = sz
+        };
+        SubmatrixIndices b22 = new SubmatrixIndices() {
+            i_min = bIndices.i_min + sz, j_min = bIndices.j_min + sz, n = sz
+        };
+        SubmatrixIndices c11 = new SubmatrixIndices() {
+            i_min = cIndices.i_min, j_min = cIndices.j_min, n = sz
+        };
+        SubmatrixIndices c12 = new SubmatrixIndices() {
+            i_min = cIndices.i_min, j_min = cIndices.j_min + sz, n = sz
+        };
+        SubmatrixIndices c21 = new SubmatrixIndices() {
+            i_min = cIndices.i_min + sz, j_min = cIndices.j_min, n = sz
+        };
+        SubmatrixIndices c22 = new SubmatrixIndices() {
+            i_min = cIndices.i_min + sz, j_min = cIndices.j_min + sz, n = sz
+        };
+        // Создание и вычисление матриц S_1, ..., S_10
+        Matrix<T>[] S = new Matrix<T>[10];
+        for (Int32 i = 0; i < S.Length; i++) {
+            S[i] = new Matrix<T>(sz, sz);
+        }
+        SubmatrixIndices sInd = new SubmatrixIndices() { i_min = 1, j_min = 1, n = sz };
+        SubmatrixSubtract(b, b12, b, b22, S[0], sInd);
+        SubmatrixSum(a, a11, a, a12, S[1], sInd);
+    }
+    private static void SubmatrixSum(Matrix<T> a, SubmatrixIndices aInd, 
+            Matrix<T> b, SubmatrixIndices bInd, Matrix<T> c, SubmatrixIndices cInd) {
+        SubmatrixArythm(a, aInd, b, bInd, c, cInd, (x, y) => { 
+            dynamic v1 = x, v2 = y; 
+            return v1 + v2; 
+        });
+    }
+    private static void SubmatrixSubtract(Matrix<T> a, SubmatrixIndices aInd,
+            Matrix<T> b, SubmatrixIndices bInd, Matrix<T> c, SubmatrixIndices cInd) {
+        SubmatrixArythm(a, aInd, b, bInd, c, cInd, (x, y) => {
+            dynamic v1 = x, v2 = y;
+            return v1 - v2;
+        });
+    }
+    private static void SubmatrixArythm(Matrix<T> a, SubmatrixIndices aInd, 
+            Matrix<T> b, SubmatrixIndices bInd, Matrix<T> c, SubmatrixIndices cInd,
+            Func<T, T, T> callback) {
+        Int32 sz = aInd.n;
+        Matrix<T> res = new Matrix<T>(sz, sz);
+        Int32 i_a = aInd.i_min, j_a = aInd.j_min, i_b = bInd.i_min, j_b = bInd.j_min;
+        for (Int32 i = 0; i < sz; i++) {
+            for (Int32 j = 0; j < sz; j++) { 
+                res[i + 1, j + 1] = callback(a[i_a + i, j_a + j], b[i_b + i, j_b + j]);
+            }
+        }
     }
     private struct SubmatrixIndices {
-        public Int32 i_max, i_min, j_max, j_min;
+        public Int32 i_min, j_min, n;
     }
 }
