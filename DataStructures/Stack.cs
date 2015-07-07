39c6e03652600a98bed6ce6916539dd7fec403f4
maksimn <maksim1989sovunion@yandex.ru>
2015-07-07 14:48:38 +0300

    Beginning Data Structures. Stack.

---
+++ DataStructures/Stack.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace DataStructures {
+    public class Stack<T> {
+        private Int32 top;
+        private T[] array;
+        private Int32 maxSize = 100;
+        public Stack() {
+            InternalArrayInitializer();
+        }
+        public Stack(Int32 size) {
+            if (size > 0) {
+                maxSize = size;
+                InternalArrayInitializer();
+            } else {
+                throw new ArgumentOutOfRangeException("size", 
+                    "size argument must be integer that is greater than zero."
+                    );
+            }           
+        }
+        private void InternalArrayInitializer() {
+            array = new T[maxSize];
+        }
+        public Boolean IsEmpty {
+            get {
+                if (top == 0) {
+                    return true;
+                }
+                return false;
+            }
+        }
+    }
+}
